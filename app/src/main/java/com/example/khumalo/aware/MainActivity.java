d9d71d204cf14b10f65e8a6d440ecb7171eec739
ComfortC <comfortchino@gmail.com>
2016-07-14 17:27:57 +0200

    Done Camera Movement

--- app/src/main/java/com/example/khumalo/aware/MainActivity.java
+++ app/src/main/java/com/example/khumalo/aware/MainActivity.java
@@ -17,35 +17,47 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 
+import java.util.DoubleSummaryStatistics;
+
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     GoogleMap m_map;
     Button moveNextLocation;
-
     LatLng currentCameraPosition;
+    CameraPosition current;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         moveNextLocation = (Button)findViewById(R.id.testing_move_next_location);
-        currentCameraPosition = new LatLng(-33.9891809,18.4832172);
+
+        moveNextLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentCameraPosition = new LatLng(-33.9659602,18.4688974);
+                updatePosition(currentCameraPosition);
+            }
+        });
 
 
         MapFragment mapFragment = (MapFragment)getFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
+    private void updatePosition(LatLng currentCameraPosition) {
+        current = new CameraPosition.Builder()
+                .target(currentCameraPosition)
+                .zoom(14)
+                .build();
+
+        m_map.animateCamera(CameraUpdateFactory.newCameraPosition(current),5000,null);
+    }
+
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
        m_map = googleMap;
-        CameraPosition current = new CameraPosition.Builder()
-                .target(currentCameraPosition)
-                .zoom(12)
-                .build();
 
-        m_map.animateCamera(CameraUpdateFactory.newCameraPosition(current),5000,null);
-
-    }
+   }
 }
