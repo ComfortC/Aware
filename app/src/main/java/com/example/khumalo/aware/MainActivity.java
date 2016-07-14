7187446077995328d155eb1819c11d81aaa12223
ComfortC <comfortchino@gmail.com>
2016-07-14 16:48:57 +0200

    Added Camera View Settings

--- app/src/main/java/com/example/khumalo/aware/MainActivity.java
+++ app/src/main/java/com/example/khumalo/aware/MainActivity.java
@@ -8,15 +8,44 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    GoogleMap m_map;
+    Button moveNextLocation;
+
+    LatLng currentCameraPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        moveNextLocation = (Button)findViewById(R.id.testing_move_next_location);
+        currentCameraPosition = new LatLng(-33.9891809,18.4832172);
 
+
+        MapFragment mapFragment = (MapFragment)getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+       m_map = googleMap;
+        CameraPosition current = new CameraPosition.Builder()
+                .target(currentCameraPosition)
+                .zoom(12)
+                .build();
+
+        m_map.animateCamera(CameraUpdateFactory.newCameraPosition(current),5000,null);
+
+    }
 }
