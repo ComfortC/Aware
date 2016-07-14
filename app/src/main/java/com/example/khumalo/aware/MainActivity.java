b18d3afb3d4f13bde64845477c42b1d5135f511a
ComfortC <comfortchino@gmail.com>
2016-07-14 18:42:22 +0200

    Added Location APi

--- app/src/main/java/com/example/khumalo/aware/MainActivity.java
+++ app/src/main/java/com/example/khumalo/aware/MainActivity.java
@@ -5,52 +5,113 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
-
+import android.location.Location;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
 import java.util.DoubleSummaryStatistics;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback,GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     GoogleMap m_map;
     Button moveNextLocation;
     LatLng currentCameraPosition;
     CameraPosition current;
+    MarkerOptions yourRide;
+    Marker now;
+
+    protected GoogleApiClient mGoogleApiClient;
+    protected Location mLastLocation;
+    protected LocationRequest mLocationRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        moveNextLocation = (Button)findViewById(R.id.testing_move_next_location);
+        yourRide= new MarkerOptions()
+                .position(new LatLng(-33.983964,18.4648159))
+                .title("Your Ride")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ride));
 
-        moveNextLocation.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                currentCameraPosition = new LatLng(-33.9659602,18.4688974);
-                updatePosition(currentCameraPosition);
-            }
-        });
 
 
         MapFragment mapFragment = (MapFragment)getFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        buildGoodleApiClient();
     }
+
+    private void buildGoodleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mGoogleApiClient.isConnected()){
+            mGoogleApiClient.disconnect();
+        }
+
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+        mLocationRequest = LocationRequest.create()
+                .setInterval(1000)
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i("Log", "GoogleApiClient connection has been suspend");
+    }
+
+
 
     private void updatePosition(LatLng currentCameraPosition) {
         current = new CameraPosition.Builder()
                 .target(currentCameraPosition)
                 .zoom(14)
                 .build();
-
+        m_map.clear();
+        yourRide= new MarkerOptions()
+                .position(currentCameraPosition)
+                .title("Your Ride")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ride));
+        m_map.addMarker(yourRide);
         m_map.animateCamera(CameraUpdateFactory.newCameraPosition(current),5000,null);
     }
 
@@ -58,6 +119,20 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
        m_map = googleMap;
-
+        m_map.addMarker(yourRide);
    }
+
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mLastLocation = location;
+        currentCameraPosition = new LatLng(mLastLocation.getLatitude(),mLastLocation.getLongitude());
+        updatePosition(currentCameraPosition);
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.i("Log", "GoogleApiClient connection has failed");
+    }
 }
