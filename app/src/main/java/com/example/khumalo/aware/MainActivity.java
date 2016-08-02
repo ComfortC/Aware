4ff03428bf09df7316976219388e96e046da0743
ComfortC <comfortchino@gmail.com>
2016-08-02 15:07:36 +0200

    Added Permission Request at Runtime

--- app/src/main/java/com/example/khumalo/aware/MainActivity.java
+++ app/src/main/java/com/example/khumalo/aware/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.khumalo.aware;
 
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import com.example.khumalo.aware.Utils.PermissionUtils;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.places.Places;
@@ -15,7 +20,8 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
 
    GoogleApiClient mGoogleApiClient;
     private static final String Tag = "Tag";
-
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    private boolean mPermissionDenied = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,6 +61,15 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     @Override
     public void onConnected(Bundle bundle) {
         Log.d(Tag, "The client has been connected");
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission to access the location is missing.
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION, true);
+
+        }else{
+            Log.d(Tag, "The Location Access has been Granted");
+        }
     }
 
     @Override
@@ -63,6 +78,41 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     }
 
 
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mPermissionDenied) {
+            // Permission was not granted, display error dialog.
+            showMissingPermissionError();
+            mPermissionDenied = false;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            return;
+        }
+
+        if (PermissionUtils.isPermissionGranted(permissions, grantResults,
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+            Log.d(Tag, "The Location Access has been Granted");
+
+        } else {
+            // Display the missing permission error dialog when the fragments resume.
+            mPermissionDenied = true;
+        }
+    }
+
+    /**
+     * Displays a dialog with error message explaining that the location permission is missing.
+     */
+    private void showMissingPermissionError() {
+        PermissionUtils.PermissionDeniedDialog
+                .newInstance(true).show(getSupportFragmentManager(), "dialog");
+    }
 }
 
 
