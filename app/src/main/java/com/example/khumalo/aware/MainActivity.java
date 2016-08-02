e33b1029b6d18b91905ebfc7ab3223d0292d331e
ComfortC <comfortchino@gmail.com>
2016-08-02 15:23:19 +0200

    Implemented place picker

--- app/src/main/java/com/example/khumalo/aware/MainActivity.java
+++ app/src/main/java/com/example/khumalo/aware/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.khumalo.aware;
 
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 
@@ -9,11 +10,17 @@ import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.khumalo.aware.Utils.PermissionUtils;
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlacePicker;
 
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks {
@@ -21,12 +28,16 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
    GoogleApiClient mGoogleApiClient;
     private static final String Tag = "Tag";
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    private static final  int PLACE_PICKER_REQUEST = 2;
     private boolean mPermissionDenied = false;
+
+    TextView placeDisplay;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         buildGoogleClient();
+        placeDisplay = (TextView)findViewById(R.id.picked_place_display_textView);
     }
 
     private void buildGoogleClient() {
@@ -55,7 +66,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
 
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
-         Log.d(Tag, "Connection has failed");
+        Log.d(Tag, "Connection has failed");
     }
 
     @Override
@@ -69,6 +80,18 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
 
         }else{
             Log.d(Tag, "The Location Access has been Granted");
+            buildPlacePicker();
+        }
+    }
+
+    private void buildPlacePicker() {
+        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+        try {
+            startActivityForResult(builder.build(this), PLACE_PICKER_REQUEST);
+        } catch (GooglePlayServicesRepairableException e) {
+            e.printStackTrace();
+        } catch (GooglePlayServicesNotAvailableException e) {
+            e.printStackTrace();
         }
     }
 
@@ -99,6 +122,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
         if (PermissionUtils.isPermissionGranted(permissions, grantResults,
                 Manifest.permission.ACCESS_FINE_LOCATION)) {
             Log.d(Tag, "The Location Access has been Granted");
+            buildPlacePicker();
 
         } else {
             // Display the missing permission error dialog when the fragments resume.
@@ -112,6 +136,17 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     private void showMissingPermissionError() {
         PermissionUtils.PermissionDeniedDialog
                 .newInstance(true).show(getSupportFragmentManager(), "dialog");
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlacePicker.getPlace(this,data);
+                String toastMsg = String.format("Place: %s", place.getName());
+                placeDisplay.setText(toastMsg);
+                Toast.makeText(this, toastMsg, Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
 
