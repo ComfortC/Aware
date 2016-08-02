6bd85b457aee3b46ef50d84dc19d06a9906b5abe
ComfortC <comfortchino@gmail.com>
2016-08-02 14:49:31 +0200

    Setting Up The API

--- app/src/main/java/com/example/khumalo/aware/MainActivity.java
+++ app/src/main/java/com/example/khumalo/aware/MainActivity.java
@@ -4,23 +4,63 @@ package com.example.khumalo.aware;
 import android.os.Bundle;
 
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.places.Places;
 
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks {
 
-
+   GoogleApiClient mGoogleApiClient;
+    private static final String Tag = "Tag";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buildGoogleClient();
+    }
+
+    private void buildGoogleClient() {
+        mGoogleApiClient = new GoogleApiClient
+                .Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .enableAutoManage(this, this)
+                .build();
 
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
+    }
 
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+         Log.d(Tag, "Connection has failed");
+    }
 
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.d(Tag, "The client has been connected");
+    }
 
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(Tag, "The connection has been suspended");
+    }
 
 
 }
